ae2c5f3188a9ece932c332408814435aa41e4104
sleepyyuu <k28143995@gmail.com>
2022-08-10 13:00:30 -0700

    Mobile only stylings

--- components/Game/GameScoreBoard.tsx
+++ components/Game/GameScoreBoard.tsx
@@ -1,10 +1,16 @@
 import styles from "../../styles/GameScoreBoard.module.scss";
+import Image from "next/image";
 
 export default function GameScoreBoard(props) {
   const { userScore, userLife } = props;
   return (
     <div className={styles.scoreBoardContainer}>
-      <div className={styles.scoreContainer}>Score: {userScore}</div>
+      <div className={styles.scoreContainer}>
+        Score: {userScore}
+        <div className={styles.goldIconContainer}>
+          <Image src={require("../../assets/sprites/gold.png")} width={30} height={30} className={styles.goldIcon} alt="Gold"></Image>
+        </div>
+      </div>
       <div className={styles.lifeContainer}>Lives Left: {userLife}</div>
     </div>
   );
