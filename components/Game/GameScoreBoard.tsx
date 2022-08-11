3ab2a8b0b34a24f8d997100322f9aa4d42621239
sleepyyuu <k28143995@gmail.com>
2022-08-11 13:52:53 -0700

    Scoreboard styling

--- components/Game/GameScoreBoard.tsx
+++ components/Game/GameScoreBoard.tsx
@@ -3,15 +3,26 @@ import Image from "next/image";
 
 export default function GameScoreBoard(props) {
   const { userScore, userLife } = props;
+  let lifeIconArray = [];
+  for (let i = 0; i < userLife; i++) {
+    lifeIconArray.push(
+      <Image src={require("../../assets/sprites/healthicon.png")} width={30} height={30} className={styles.goldIcon} alt="Life"></Image>
+    );
+  }
   return (
     <div className={styles.scoreBoardContainer}>
       <div className={styles.scoreContainer}>
-        Score: {userScore}
+        SCORE <div className={styles.scoreNumber}>&nbsp;{userScore}</div>
         <div className={styles.goldIconContainer}>
-          <Image src={require("../../assets/sprites/gold.png")} width={30} height={30} className={styles.goldIcon} alt="Gold"></Image>
+          <Image src={require("../../assets/sprites/gold.png")} width={30} height={30} className={styles.healthIcon} alt="Gold"></Image>
         </div>
       </div>
-      <div className={styles.lifeContainer}>Lives Left: {userLife}</div>
+      <div className={styles.lifeContainer}>
+        {lifeIconArray.map((icon, index) => {
+          return <div key={index}>{icon}</div>;
+        })}
+        <div className={styles.livesWord}>&nbsp;LIVES</div>
+      </div>
     </div>
   );
 }
