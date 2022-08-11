7c2182b19e26d07a623cb2b20a9e27065ee1d016
sleepyyuu <k28143995@gmail.com>
2022-08-11 02:35:01 -0700

    Update popup

--- components/Game/Game.tsx
+++ components/Game/Game.tsx
@@ -186,6 +186,12 @@ export default function Game() {
           height: isMobile ? "70%" : "45rem",
           padding: "0px",
           animation: "anvil 0.3s cubic-bezier(0.38, 0.1, 0.36, 0.9) forwards",
+          position: "fixed",
+
+          top: "0",
+          bottom: "0",
+          left: "0",
+          right: "0",
         }}
         modal
         className={styles.popupScreen}
