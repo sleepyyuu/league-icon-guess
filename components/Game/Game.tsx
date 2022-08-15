f84f0810d436e10dd580cae850773de5b67b0da0
sleepyyuu <k28143995@gmail.com>
2022-08-15 11:05:50 -0700

    Firebase config

--- components/Game/Game.tsx
+++ components/Game/Game.tsx
@@ -158,17 +158,18 @@ export default function Game() {
     }
   }, [gameCount]);
 
+  const firebaseConfig = {
+    apiKey: "AIzaSyByPoeWhQtqjn1gZ_Bobg9uHPHoq5UfcYk",
+    authDomain: "league-skill-issue.firebaseapp.com",
+    projectId: "league-skill-issue",
+    storageBucket: "league-skill-issue.appspot.com",
+    messagingSenderId: "205982962159",
+    appId: "1:205982962159:web:dc37af54a61e4129d7a61a",
+    measurementId: "G-XNM7NS09ZS",
+  };
+  const app = initializeApp(firebaseConfig);
+
   useEffect(() => {
-    const firebaseConfig = {
-      apiKey: "AIzaSyByPoeWhQtqjn1gZ_Bobg9uHPHoq5UfcYk",
-      authDomain: "league-skill-issue.firebaseapp.com",
-      projectId: "league-skill-issue",
-      storageBucket: "league-skill-issue.appspot.com",
-      messagingSenderId: "205982962159",
-      appId: "1:205982962159:web:dc37af54a61e4129d7a61a",
-      measurementId: "G-XNM7NS09ZS",
-    };
-    const app = initializeApp(firebaseConfig);
     const analytics = getAnalytics(app);
     logEvent(analytics, "visited");
     setShowInitialMenu(true);
