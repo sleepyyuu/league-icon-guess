c8f0df70e6e25363b5569bcbdaf6af73b2161e9f
sleepyyuu <k28143995@gmail.com>
2022-08-17 12:02:43 -0700

    Modal centering mobile height

--- components/Game/Game.tsx
+++ components/Game/Game.tsx
@@ -209,7 +209,7 @@ export default function Game() {
           borderRadius: "7px",
           width: isMobile ? "95vw" : "33rem",
           maxWidth: "33rem",
-          height: isMobile ? "75vh" : "45rem",
+          height: isMobile ? "" : "45rem",
           padding: "0px",
           animation: "anvil 0.3s cubic-bezier(0.38, 0.1, 0.36, 0.9) forwards",
         }}
@@ -273,7 +273,7 @@ export default function Game() {
           borderRadius: "7px",
           width: isMobile ? "95vw" : "33rem",
           maxWidth: "33rem",
-          height: isMobile ? "75vh" : "45rem",
+          height: isMobile ? "" : "45rem",
           padding: "0px",
           animation: "anvil 0.3s cubic-bezier(0.38, 0.1, 0.36, 0.9) forwards",
         }}
