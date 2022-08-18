e6a56807ba87bf4706c6616d2d5c39b6a686ed4b
sleepyyuu <k28143995@gmail.com>
2022-08-17 17:07:07 -0700

    Initialize firebase only once

--- components/Game/Game.tsx
+++ components/Game/Game.tsx
@@ -31,17 +31,6 @@ export default function Game() {
   const [highestStreak, setHighestStreak] = useState(0);
   const [numGamesPlayed, setNumGamesPlayed] = useState(0);
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyByPoeWhQtqjn1gZ_Bobg9uHPHoq5UfcYk",
-    authDomain: "league-skill-issue.firebaseapp.com",
-    projectId: "league-skill-issue",
-    storageBucket: "league-skill-issue.appspot.com",
-    messagingSenderId: "205982962159",
-    appId: "1:205982962159:web:dc37af54a61e4129d7a61a",
-    measurementId: "G-XNM7NS09ZS",
-  };
-  const app = initializeApp(firebaseConfig);
-
   const championArray = Object.keys(championListData.data);
   const exampleAbilities = [
     <img
@@ -175,6 +164,16 @@ export default function Game() {
   }, [gameCount]);
 
   useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyByPoeWhQtqjn1gZ_Bobg9uHPHoq5UfcYk",
+      authDomain: "league-skill-issue.firebaseapp.com",
+      projectId: "league-skill-issue",
+      storageBucket: "league-skill-issue.appspot.com",
+      messagingSenderId: "205982962159",
+      appId: "1:205982962159:web:dc37af54a61e4129d7a61a",
+      measurementId: "G-XNM7NS09ZS",
+    };
+    const app = initializeApp(firebaseConfig);
     setShowInitialMenu(true);
     pullFromLocalStorage();
     setShowButtons(true);
