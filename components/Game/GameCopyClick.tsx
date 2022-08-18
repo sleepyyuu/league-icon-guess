6aba18fd7feef8b29c7fcfea7914ba0dce50f1ed
sleepyyuu <k28143995@gmail.com>
2022-08-17 17:57:03 -0700

    Comment out sharelink event

--- components/Game/GameCopyClick.tsx
+++ components/Game/GameCopyClick.tsx
@@ -7,16 +7,16 @@ import { initializeApp } from "firebase/app";
 export default function GameCopyClick() {
   const [copyText, setCopyText] = useState(false);
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyByPoeWhQtqjn1gZ_Bobg9uHPHoq5UfcYk",
-    authDomain: "league-skill-issue.firebaseapp.com",
-    projectId: "league-skill-issue",
-    storageBucket: "league-skill-issue.appspot.com",
-    messagingSenderId: "205982962159",
-    appId: "1:205982962159:web:dc37af54a61e4129d7a61a",
-    measurementId: "G-XNM7NS09ZS",
-  };
-  const app = initializeApp(firebaseConfig);
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyByPoeWhQtqjn1gZ_Bobg9uHPHoq5UfcYk",
+  //   authDomain: "league-skill-issue.firebaseapp.com",
+  //   projectId: "league-skill-issue",
+  //   storageBucket: "league-skill-issue.appspot.com",
+  //   messagingSenderId: "205982962159",
+  //   appId: "1:205982962159:web:dc37af54a61e4129d7a61a",
+  //   measurementId: "G-XNM7NS09ZS",
+  // };
+  // const app = initializeApp(firebaseConfig);
 
   useEffect(() => {
     let timeoutId = setTimeout(() => {
@@ -35,8 +35,8 @@ export default function GameCopyClick() {
         onClick={() => {
           navigator.clipboard.writeText("https://skill-issue.com");
           setCopyText(true);
-          const analytics = getAnalytics(app);
-          logEvent(analytics, "share_link_clicked");
+          // const analytics = getAnalytics(app);
+          // logEvent(analytics, "share_link_clicked");
         }}
       >
         <FaCopy className={styles.copyUrlIcon}></FaCopy>
